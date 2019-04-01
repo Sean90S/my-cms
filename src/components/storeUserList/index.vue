c73b2acd1174acfc8c4e9c7f026dcf98a27fbc96
Sean90S <m17087791692@163.com>
2019-04-01 09:50:55 +0800

    lk

--- src/components/storeUserList/index.vue
+++ src/components/storeUserList/index.vue
@@ -66,7 +66,7 @@
         name: "index",
         data() {
             return {
-                blood: false,
+                blood: true,
                 lockerVisible: false,
                 lockerVisible1: false,
             }
